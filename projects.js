efabd8ad036057a8ddec4f4af08db1d1213f4c72
hala slama <slamahala@gmail.com>
2025-05-22 23:33:05 +0200

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,17 @@
+var PROJECTS = [
+    {
+        img: "images/images1.jpg",
+        title:"Project1",
+        description:"Fitness Coaching Project"
+    },{
+        img: "images/images2.jpg",
+        title:"Project2",
+        description:"To-Do List App"
+    },
+    {
+        img: "images/images3.jpg",
+        title:"Project3",
+        description:"Car Leasing Project"
+    },
+
+]
