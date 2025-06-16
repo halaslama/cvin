32430f8306cde26afafd87ed6d47e7efcc886f79
hala slama <slamahala@gmail.com>
2025-06-16 14:16:42 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,17 +1,18 @@
 var PROJECTS = [
     {
-        img: "images/images1.jpg",
+        img: "images1.jpg",
         title:"Project1",
         description:"Fitness Coaching Project"
-    },{
-        img: "images/images2.jpg",
+    },
+    {
+        img: "images2.jpg",
         title:"Project2",
         description:"To-Do List App"
     },
     {
-        img: "images/images3.jpg",
+        img: "images3.jpg",
         title:"Project3",
         description:"Car Leasing Project"
-    },
+    }
 
 ]
